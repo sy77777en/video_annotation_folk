a0a34bf3543c9064e5ba3b05e883f8a8df27bb88
linzhiqiu <zl279@cornell.edu>
2025-07-30 00:49:33 -0400

    [add] fix wrong copy link issue

--- caption/utils.py
+++ caption/utils.py
@@ -42,12 +42,12 @@ def extract_frames(video_path: str, frame_numbers: List[int]) -> List[np.ndarray
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
-        try:
-            local_video_path = "video.mp4"
-            urllib.request.urlretrieve(video_path, local_video_path)
-            cap = cv2.VideoCapture(local_video_path)
-        except:
-            raise ValueError(f"Error: Unable to open video {video_path}")
+        # try:
+        #     local_video_path = "video.mp4"
+        #     urllib.request.urlretrieve(video_path, local_video_path)
+        #     cap = cv2.VideoCapture(local_video_path)
+        # except:
+        raise ValueError(f"Error: Unable to open video {video_path}")
 
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
